6df5a09f6c217bb608dd5eceec08cc214111cebb
stefanopisciella <stefano.pisciella1@student.univaq.it>
2023-09-29 12:57:32 +0200

    m

---
+++ application/controllers/Responsive.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers;
+
+require "application/controllers/AbstractController.php";
+
+class Responsive extends AbastractController{
+    public static function index() {
+    
+    }
+}
